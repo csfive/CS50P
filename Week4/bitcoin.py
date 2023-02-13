47528a66207a40a3e42934789ad9f74258962d82
mancuoj <1337381768@qq.com>
2023-02-14 03:18:41 +0800

    refactor: no function!

--- Week4/bitcoin.py
+++ Week4/bitcoin.py
@@ -2,24 +2,18 @@ import sys
 import requests
 
 
-def get_btc_price():
-    try:
-        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        data = response.json()
-        return data["bpi"]["USD"]["rate_float"]
-    except requests.RequestException:
-        sys.exit()
+try:
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    data = response.json()
+    current_price = data["bpi"]["USD"]["rate_float"]
+except requests.RequestException:
+    sys.exit()
 
 
-def main():
-    try:
-        n = float(sys.argv[1])
-    except IndexError:
-        sys.exit("Missing command-line argument")
-    except ValueError:
-        sys.exit("Command-line argument is not a number")
-    print(f"${n * get_btc_price():,.4f}")
-
-
-if __name__ == "__main__":
-    main()
+try:
+    n = float(sys.argv[1])
+except IndexError:
+    sys.exit("Missing command-line argument")
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+print(f"${n * current_price:,.4f}")
