0f01bc1543fa0eb7aa946a4744d3ee700724b9fb
mancuoj <1337381768@qq.com>
2023-03-19 22:41:45 +0800

    fix: incomplete code uploaded earlier

--- Week5/test_plates/test_plates.py
+++ Week5/test_plates/test_plates.py
@@ -1,12 +1,13 @@
 from plates import is_valid
 
-
 def test_is_valid():
     assert is_valid("CS50") == True
     assert is_valid("ECTO88") == True
     assert is_valid("NRVOUS") == True
     assert is_valid("CS05") == False
     assert is_valid("CS50P2") == False
+    assert is_valid("123456") == False
     assert is_valid("PI3.14") == False
     assert is_valid("H") == False
     assert is_valid("OUTATIME") == False
+    assert is_valid("ABC?!-") == False
